a085919e3fb260b321e587bba6f928c17232ea05
Drywno <drywno@gmail.com>
2024-12-17 10:22:16 +0100

    style aplikacji

---
+++ LoginPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using flashker.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static flashker.ViewModels.LoginViewModel;
+
+namespace flashker.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+
+   
+    private async void OnRegisterClicked(object sender, EventArgs e)
+        {
+            // Przechodzi do strony rejestracji
+            await Navigation.PushAsync(new RegisterPage());
+        }
+    }
+}
